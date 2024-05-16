049f9de2724492023642b3448cade69e2f1f44cf
Aprilend <msiec45@gmail.com>
2024-05-16 16:22:32 +0900

    웹페이지 기본 세팅 commit

---
+++ basicBoard/src/main/java/com/macaronimusic/basicBoard/home/web/homeController.java
@@ -0,0 +1,13 @@
+package com.macaronimusic.basicBoard.home.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+}
