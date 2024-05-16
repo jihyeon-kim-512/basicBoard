fc5efbffb2bded83cf4ab02b1c17dc8aa95fa3d4
Aprilend <msiec45@gmail.com>
2024-05-16 16:24:55 +0900

    엑셀 파일 업로드 view, controller commit

---
+++ basicBoard/src/main/java/com/macaronimusic/basicBoard/adminUpload/web/adminUploadController.java
@@ -0,0 +1,26 @@
+package com.macaronimusic.basicBoard.adminUpload.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Controller
+public class adminUploadController {
+
+	@GetMapping("/admin-upload")
+	public String adminUpload() {
+		return "adminUpload";
+	}
+	
+	@PostMapping("/admin-upload/uploadexcel")
+	public String readExcel(@RequestParam("file") MultipartFile file, Model model) {
+		//TODO: process POST request
+		
+		return "excelList";
+	}
+	
+}
