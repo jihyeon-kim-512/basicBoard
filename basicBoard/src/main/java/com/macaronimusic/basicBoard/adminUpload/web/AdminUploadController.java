7b377aae2c5b8a0b039a62578ea369514cf122a8
Aprilend <msiec45@gmail.com>
2024-05-17 15:03:09 +0900

    엑셀 마지막 행 확인 코드 추가

--- basicBoard/src/main/java/com/macaronimusic/basicBoard/adminUpload/web/AdminUploadController.java
+++ basicBoard/src/main/java/com/macaronimusic/basicBoard/adminUpload/web/AdminUploadController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.macaronimusic.basicBoard.adminUpload.dto.ExcelData;
+import com.macaronimusic.basicBoard.adminUpload.dto.adminUploadDTO;
 
 @Controller
 public class AdminUploadController {
@@ -28,11 +29,11 @@ public class AdminUploadController {
 	}
 
 	@PostMapping("admin-upload/uploadexcel")
-	public String readExcel(@RequestParam("file") MultipartFile file, Model model) throws IOException { // 2
+	public String readExcel(@RequestParam("file") MultipartFile file, Model model) throws IOException {
 
-		List<ExcelData> dataList = new ArrayList<>();
+		List<adminUploadDTO> dataList = new ArrayList<>();
 
-		String extension = FilenameUtils.getExtension(file.getOriginalFilename()); // 3
+		String extension = FilenameUtils.getExtension(file.getOriginalFilename());
 
 		if (!extension.equals("xlsx") && !extension.equals("xls")) {
 			throw new IOException("엑셀파일만 업로드 해주세요.");
@@ -48,20 +49,45 @@ public class AdminUploadController {
 
 		Sheet worksheet = workbook.getSheetAt(0);
 
-		for (int i = 1; i < worksheet.getPhysicalNumberOfRows(); i++) { // 4
+		for (int i = 1; i < worksheet.getPhysicalNumberOfRows(); i++) {
 
 			Row row = worksheet.getRow(i);
 
-			ExcelData data = new ExcelData();
+			adminUploadDTO data = new adminUploadDTO();
+			String data_exist = "";
+
+			// 마지막 행 확인
+			for (Cell cell : row) {
+				data_exist += cell;
+			}
+
+			if (data_exist.equals(""))
+				break;
 
 			data.setTitle(row.getCell(0).getStringCellValue());
 			data.setArtist(row.getCell(1).getStringCellValue());
-			
-			
-			dataList.add(data);
+			data.setVocal(row.getCell(2).getStringCellValue());
+			data.setFeaturing(row.getCell(3).getStringCellValue());
+			data.setComposer(row.getCell(4).getStringCellValue());
+			data.setLyricist(row.getCell(5).getStringCellValue());
+			data.setArranger(row.getCell(6).getStringCellValue());
+			data.setAlbum(row.getCell(7).getStringCellValue());
+			data.setPlaytime(row.getCell(8).getStringCellValue());
+			data.setFilename(row.getCell(9).getStringCellValue());
+
+			// row.getCell(0).getCellType() == 3 셀의 데이터 유형이 BLANK임
+			if (row.getCell(0).getCellType() == 3 || row.getCell(1).getCellType() == 3
+					|| row.getCell(2).getCellType() == 3 || row.getCell(4).getCellType() == 3
+					|| row.getCell(5).getCellType() == 3 || row.getCell(7).getCellType() == 3) {
+				continue;
+
+			} else {
+				dataList.add(data);
+			}
+
 		}
 
-		model.addAttribute("datas", dataList); // 5
+		model.addAttribute("datas", dataList);
 
 		return "excelList";
 
