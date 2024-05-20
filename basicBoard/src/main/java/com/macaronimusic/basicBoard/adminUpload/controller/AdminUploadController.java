3071d6abf336b5072b8b900168cc53f9233a9a31
jihyeon-kim-512 <jihyeon-kim-512@gmail.com>
2024-05-20 21:23:53 +0900

    관리자 엑셀 업로드 기능 수정

--- basicBoard/src/main/java/com/macaronimusic/basicBoard/adminUpload/controller/AdminUploadController.java
+++ basicBoard/src/main/java/com/macaronimusic/basicBoard/adminUpload/controller/AdminUploadController.java
@@ -1,16 +1,8 @@
 package com.macaronimusic.basicBoard.adminUpload.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.io.FilenameUtils;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,73 +28,8 @@ public class AdminUploadController {
 
 	@PostMapping("/admin-upload")
 	public String readExcel(@RequestParam("file") MultipartFile file, Model model) throws IOException {
-
-		List<AdminUploadDTO> dataList = new ArrayList<>();
-
-		String extension = FilenameUtils.getExtension(file.getOriginalFilename());
-
-		if (!extension.equals("xlsx") && !extension.equals("xls")) {
-			throw new IOException("엑셀파일만 업로드 해주세요.");
-		}
-
-		Workbook workbook = null;
-
-		if (extension.equals("xlsx")) {
-			workbook = new XSSFWorkbook(file.getInputStream());
-		} else if (extension.equals("xls")) {
-			workbook = new HSSFWorkbook(file.getInputStream());
-		}
-
-		Sheet worksheet = workbook.getSheetAt(0);
-
-		for (int i = 1; i < worksheet.getPhysicalNumberOfRows(); i++) {
-
-			Row row = worksheet.getRow(i);
-
-			AdminUploadDTO data = new AdminUploadDTO();
-			String data_exist = "";
-
-			// 마지막 행 확인
-			for (Cell cell : row) {
-				data_exist += cell;
-			}
-
-			if (data_exist.equals(""))
-				break;
-
-			// dto와 entity 따로 저장하는 방법 말고 다른 방법은?
-			data.setTitle(row.getCell(0).getStringCellValue());
-			data.setArtist(row.getCell(1).getStringCellValue());
-			data.setVocal(row.getCell(2).getStringCellValue());
-			data.setFeaturing(row.getCell(3).getStringCellValue());
-			data.setComposer(row.getCell(4).getStringCellValue());
-			data.setLyricist(row.getCell(5).getStringCellValue());
-			data.setArranger(row.getCell(6).getStringCellValue());
-			data.setAlbum(row.getCell(7).getStringCellValue());
-			data.setPlaytime(row.getCell(8).getStringCellValue());
-			data.setFile_name(row.getCell(9).getStringCellValue());
-
-			// row.getCell(0).getCellType() == 3 셀의 데이터 유형이 BLANK임
-			if (row.getCell(0).getCellType() == 3 || row.getCell(1).getCellType() == 3
-					|| row.getCell(2).getCellType() == 3 || row.getCell(4).getCellType() == 3
-					|| row.getCell(5).getCellType() == 3 || row.getCell(7).getCellType() == 3) {
-				continue;
-				// 유효성 검사 후 처리 방법 생각하기
-				// 전체 취소? 오류 행 개수 출력?
-
-			} else {
-				dataList.add(data);
-				// DB 저장 전에 중복 확인 필요
-				// 업데이트 할 것인지? 저장하지 않을 것인지
-			}
-
-			adminUploadService.save(data);
-
-		}
-
+		List<AdminUploadDTO> dataList = adminUploadService.ReadExcel(file);
 		model.addAttribute("datas", dataList);
-
 		return "ExcelList";
-
 	}
 }
--- basicBoard/src/main/java/com/macaronimusic/basicBoard/adminUpload/service/AdminUploadService.java
+++ basicBoard/src/main/java/com/macaronimusic/basicBoard/adminUpload/service/AdminUploadService.java
@@ -1,6 +1,15 @@
 package com.macaronimusic.basicBoard.adminUpload.service;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.macaronimusic.basicBoard.adminUpload.dto.AdminUploadDTO;
 import com.macaronimusic.basicBoard.adminUpload.model.AdminUploadEntity;
@@ -14,9 +23,61 @@ public class AdminUploadService {
 
 	private final AdminUploadRepository adminUploadRepository;
 
-	public void save(AdminUploadDTO adminUploadDTO) {
-		AdminUploadEntity adminUploadEntity = AdminUploadEntity.toAdminUploadEntity(adminUploadDTO);
-		adminUploadRepository.save(adminUploadEntity);
-	}
+	public List<AdminUploadDTO> ReadExcel(MultipartFile file) throws IOException {
+		List<AdminUploadDTO> dataList = new ArrayList<>();
+		String extension = FilenameUtils.getExtension(file.getOriginalFilename());
 
+		if (!extension.equals("xlsx") && !extension.equals("xls")) {
+			throw new IOException("엑셀파일만 업로드 해주세요.");
+		}
+
+		Workbook workbook = extension.equals("xlsx") ? new XSSFWorkbook(file.getInputStream())
+				: new HSSFWorkbook(file.getInputStream());
+		Sheet worksheet = workbook.getSheetAt(0);
+
+		for (int i = 1; i < worksheet.getPhysicalNumberOfRows(); i++) {
+			Row row = worksheet.getRow(i);
+
+			AdminUploadDTO data = new AdminUploadDTO();
+			String data_exist = "";
+
+			// 마지막 행 확인
+			for (Cell cell : row) {
+				data_exist += cell;
+			}
+
+			if (data_exist.equals(""))
+				break;
+
+			data.setTitle(row.getCell(0).getStringCellValue());
+			data.setArtist(row.getCell(1).getStringCellValue());
+			data.setVocal(row.getCell(2).getStringCellValue());
+			data.setFeaturing(row.getCell(3).getStringCellValue());
+			data.setComposer(row.getCell(4).getStringCellValue());
+			data.setLyricist(row.getCell(5).getStringCellValue());
+			data.setArranger(row.getCell(6).getStringCellValue());
+			data.setAlbum(row.getCell(7).getStringCellValue());
+			data.setPlaytime(row.getCell(8).getStringCellValue());
+			data.setFile_name(row.getCell(9).getStringCellValue());
+
+			// row.getCell(0).getCellType() == 3 셀의 데이터 유형이 BLANK임
+			if (row.getCell(0).getCellType() == 3 || row.getCell(1).getCellType() == 3
+					|| row.getCell(2).getCellType() == 3 || row.getCell(4).getCellType() == 3
+					|| row.getCell(5).getCellType() == 3 || row.getCell(7).getCellType() == 3) {
+				continue;
+				// 유효성 검사 후 처리 방법 생각하기
+				// 전체 취소? 오류 행 개수 출력?
+			}
+
+			dataList.add(data);
+			
+			// DB 저장 전에 중복 확인 필요
+			// 업데이트 할 것인지? 저장하지 않을 것인지
+			
+			AdminUploadEntity entity = AdminUploadDTO.toAdminUploadEntity(data);
+			adminUploadRepository.save(entity);
+		}
+
+		return dataList;
+	}
 }
